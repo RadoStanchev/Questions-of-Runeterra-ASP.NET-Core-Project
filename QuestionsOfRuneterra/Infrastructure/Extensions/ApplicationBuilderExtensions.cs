c0cf1d52db10a75662a0c8c02b95ad0a89277386
RadoStanchev <radostanchev04@gmail.com>
2021-08-06 17:03:09 +0300

    Add Admin area and admin role

---
+++ QuestionsOfRuneterra/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using QuestionsOfRuneterra.Data;
+using QuestionsOfRuneterra.Data.Models;
+using System;
+using System.Threading.Tasks;
+using static QuestionsOfRuneterra.Areas.Admin.AdminConstants;
+
+namespace QuestionsOfRuneterra.Infrastructure.Extensions
+{
+
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder PrepareDatabase(
+            this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            var services = serviceScope.ServiceProvider;
+
+            MigrateDatabase(services);
+
+            SeedAdministrator(services);
+
+            return app;
+        }
+
+        private static void MigrateDatabase(IServiceProvider services)
+        {
+            var data = services.GetRequiredService<ApplicationDbContext>();
+
+            data.Database.Migrate();
+        }
+
+
+        private static void SeedAdministrator(IServiceProvider services)
+        {
+            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+            Task
+                .Run(async () =>
+                {
+                    if (await roleManager.RoleExistsAsync(AdministratorRoleName))
+                    {
+                        return;
+                    }
+
+                    var role = new IdentityRole { Name = AdministratorRoleName };
+
+                    await roleManager.CreateAsync(role);
+
+                    const string adminEmail = "admin@admin.com";
+                    const string adminPassword = "admin2004";
+
+                    var user = new ApplicationUser
+                    {
+                        Email = adminEmail,
+                        UserName = adminEmail,
+                        FirstName = "Admin",
+                        LastName = "Admin",
+                    };
+
+                    await userManager.CreateAsync(user, adminPassword);
+
+                    await userManager.AddToRoleAsync(user, role.Name);
+                })
+                .GetAwaiter()
+                .GetResult();
+        }
+    }
+}
---
+++ QuestionsOfRuneterra/Infrastructure/MappingProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace QuestionsOfRuneterra.Infrastructure
+{
+    using AutoMapper;
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+        }
+    }
+}
